22d31ae42001627f9ea9ec0f18d365dbc784cf9d
gitnob <github@groucho.inka.de>
2024-10-12 23:16:43 +0200

    Update MidiPerfoSeq.cpp

--- plugins/MidiPerfoSeq/MidiPerfoSeq.cpp
+++ plugins/MidiPerfoSeq/MidiPerfoSeq.cpp
@@ -454,7 +454,7 @@ protected:
                                             std::cout                 << int(me.data[2] & 0x7F) << "\n";
                                              me.data[0] = (me.data[0] & 0x0F) + 0x80;  // create a note off
                                              me.data[1] = (me.data[1] + 0x100 + transposeNote) % 0x100;
-                                             me.frame = uint32_t(i);
+                                             me.frame = uint32_t(midiEvent.frame+i);
                                              writeMidiEvent(me);
                                          }
                                          getNextSequencerIndex();
